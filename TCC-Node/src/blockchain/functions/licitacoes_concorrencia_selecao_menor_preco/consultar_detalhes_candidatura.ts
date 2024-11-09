b192b4e88101ca900cd5792252768f8da6b0958a
Azevedo-Erick <azevedoerick762@gmail.com>
2024-11-09 18:39:42 -0300

    ✨: função para buscar detalhes de candidaturas

--- TCC-Node/src/blockchain/functions/licitacoes_concorrencia_selecao_menor_preco/consultar_detalhes_candidatura.ts
+++ TCC-Node/src/blockchain/functions/licitacoes_concorrencia_selecao_menor_preco/consultar_detalhes_candidatura.ts
@@ -1,6 +1,6 @@
-import { Web3Provider } from "../../../app/providers/web3_provider";
-import container from "../../../di/container";
-import licitacao_concorrencia_selecao_menor_preco_abi from "../../contracts/abis/licitacao_concorrencia_selecao_menor_preco_abi";
+import { Web3Provider } from '../../../app/providers/web3_provider';
+import container from '../../../di/container';
+import licitacao_concorrencia_selecao_menor_preco_abi from '../../contracts/abis/licitacao_concorrencia_selecao_menor_preco_abi';
 
 interface CandidatoDetalhes {
     endereco: string;
@@ -8,20 +8,29 @@ interface CandidatoDetalhes {
     timestampEnvio: number;
 }
 
-export default async function consultarDetalhesCandidatos(contractAddress: string): Promise<CandidatoDetalhes[]> {
+export default async function consultarDetalhesCandidatos(
+    contractAddress: string
+): Promise<CandidatoDetalhes[]> {
     const web3 = container.get(Web3Provider).getWeb3();
-    const contrato = new web3.eth.Contract(licitacao_concorrencia_selecao_menor_preco_abi, contractAddress);
+    const contrato = new web3.eth.Contract(
+        licitacao_concorrencia_selecao_menor_preco_abi,
+        contractAddress
+    );
 
-    const candidatosList: string[] = await contrato.methods.getCandidatos().call();
+    const candidatosList: string[] = await contrato.methods
+        .getCandidatos()
+        .call();
 
     const candidatosDetalhes: CandidatoDetalhes[] = await Promise.all(
         candidatosList.map(async (candidatoAddress) => {
-            const candidatoDetalhes = await contrato.methods.candidatos(candidatoAddress).call() as any;
+            const candidatoDetalhes = (await contrato.methods
+                .candidatos(candidatoAddress)
+                .call()) as any;
 
             return {
                 endereco: candidatoDetalhes.endereco,
                 hashCandidatura: candidatoDetalhes.hashCandidatura,
-                timestampEnvio: parseInt(candidatoDetalhes.timestampEnvio, 10),
+                timestampEnvio: parseInt(candidatoDetalhes.timestampEnvio, 10)
             };
         })
     );
