43e22859447897d58f92665001ebd9f78f64b696
Azevedo-Erick <azevedoerick762@gmail.com>
2024-11-09 18:30:49 -0300

    🎨: adição do resto das dependencias no container de injeção de dependencia

--- TCC-Node/src/di/container.ts
+++ TCC-Node/src/di/container.ts
@@ -1,6 +1,5 @@
 import { Container } from 'inversify';
 import { StorageLicitacoesService } from '../app/services/storage_licitacoes_service';
-import { PrismaClient } from '@prisma/client';
 import { JobsScheduler } from '../jobs/jobs_scheduler';
 import { LicitacaoConcorrenciaSelecaoMenorPrecoService } from '../app/services/licitacao_concorrencia_selecao_menor_preco';
 import { Web3Provider } from '../app/providers/web3_provider';
@@ -12,8 +11,8 @@ const container = new Container();
 container.bind(Web3Provider).toConstantValue(web3Instance);
 container.bind(PrismaClientProvider).toSelf();
 
-//container.bind(StorageLicitacoesService).toSelf();
-//container.bind(LicitacaoConcorrenciaSelecaoMenorPrecoService).toSelf();
-//container.bind(JobsScheduler).toSelf();
+container.bind(StorageLicitacoesService).toSelf();
+container.bind(LicitacaoConcorrenciaSelecaoMenorPrecoService).toSelf();
+container.bind(JobsScheduler).toSelf();
 
 export default container;
