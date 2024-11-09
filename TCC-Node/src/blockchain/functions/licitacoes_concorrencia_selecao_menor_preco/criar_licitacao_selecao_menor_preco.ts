15d13cf9f4dd959575ab1c11ff045c3b584ec623
Azevedo-Erick <azevedoerick762@gmail.com>
2024-11-09 18:34:39 -0300

    🚚: separação de código e renomeação de arquivos

--- TCC-Node/src/blockchain/functions/licitacoes_concorrencia_selecao_menor_preco/criar_licitacao_selecao_menor_preco.ts
+++ TCC-Node/src/blockchain/functions/licitacoes_concorrencia_selecao_menor_preco/criar_licitacao_selecao_menor_preco.ts
@@ -1,18 +1,18 @@
 import leilao_abi from '../../contracts/abis/licitacao_concorrencia_selecao_menor_preco_abi.js';
-import licitacaoConcorrenciaSelecaoMenorPrecoBytecode from '../../contracts/bytecodes/licitacao_concorrencia_selecao_menor_preco_bytecode.js';
-import assinarTransacao from '../../utils/assinar_transacao.js';
-import { privateKey } from '../../../configs/config.js';
 import container from '../../../di/container.js';
 import Web3 from 'web3';
-import { Web3Provider } from '../../../app/providers/web3_provider.js';
 import RequestCriarLicitacaoDto from '../../dtos/request_criar_licitacao_dto.js';
+import deployLicitacaoSelecaoMenorPrecoContract from './deploy_licitacao_selecao_menor_preco_contract.js';
 
-export default async function createAuction(dto: RequestCriarLicitacaoDto) {
+export default async function criarLicitacaoSelecaoMenorPreco(
+    dto: RequestCriarLicitacaoDto
+) {
     const web3 = container.get<Web3>(Web3);
 
     const {
-        from,
+        enderecoRemetente: from,
         titulo,
+        chaveRemetente,
         descricao,
         hashETP,
         hashEdital,
@@ -21,7 +21,10 @@ export default async function createAuction(dto: RequestCriarLicitacaoDto) {
         dataFimCandidaturas
     } = dto;
 
-    const transaction = await deployAuctionContract(from);
+    const transaction = await deployLicitacaoSelecaoMenorPrecoContract(
+        from,
+        chaveRemetente
+    );
 
     if (!transaction) {
         return;
@@ -55,32 +58,13 @@ export default async function createAuction(dto: RequestCriarLicitacaoDto) {
         gas: 2000000
     };
 
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    const signedTx = await web3.eth.accounts.signTransaction(
+        tx,
+        chaveRemetente
+    );
 
     const receipt = await web3.eth.sendSignedTransaction(
         signedTx.rawTransaction
     );
     return receipt.blockHash;
 }
-
-const deployAuctionContract = async (from: string) => {
-    const web3 = container.get(Web3Provider).getWeb3();
-
-    const contrato = new web3.eth.Contract(leilao_abi);
-    const deploy = contrato
-        .deploy({
-            data: licitacaoConcorrenciaSelecaoMenorPrecoBytecode,
-            arguments: []
-        })
-        .encodeABI();
-
-    const objetoTransacao = {
-        data: deploy,
-        from: from,
-        gas: 2273364,
-        gasPrice: 0
-    };
-
-    const rawTx = await assinarTransacao(objetoTransacao, privateKey);
-    return await web3.eth.sendSignedTransaction(rawTx);
-};
