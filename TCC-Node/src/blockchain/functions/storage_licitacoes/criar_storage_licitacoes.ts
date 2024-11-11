3a143640a3f528ee6f91fc59a25531610b8b1812
Azevedo-Erick <azevedoerick762@gmail.com>
2024-11-10 22:40:27 -0300

    ✨: funcionalidade de instanciar o contrato de storage de endereços

--- TCC-Node/src/blockchain/functions/storage_licitacoes/criar_storage_licitacoes.ts
+++ TCC-Node/src/blockchain/functions/storage_licitacoes/criar_storage_licitacoes.ts
@@ -1,28 +1,36 @@
 import assinarTransacao from '../../utils/assinar_transacao.js';
-import { privateKey } from '../../../configs/config.js';
 import storage_licitacoes_abi from '../../contracts/abis/storage_licitacoes_abi.js';
 import storage_licitacoes_bytecode from '../../contracts/bytecodes/storage_licitacoes_bytecode.js';
 import container from '../../../di/container.js';
-import Web3 from 'web3';
+import { Web3Provider } from '../../../app/providers/web3_provider.js';
 
-export default async function criarStorageLicitacoes(from: string) {
-    const web3 = container.get<Web3>(Web3);
+export default async function criarStorageLicitacoes(
+    from: string,
+    privateKey: string
+) {
+    const web3 = container.get<Web3Provider>(Web3Provider).getWeb3();
 
     const contrato = new web3.eth.Contract(storage_licitacoes_abi);
-    const deploy = contrato
-        .deploy({
-            data: storage_licitacoes_bytecode,
-            arguments: []
-        })
-        .encodeABI();
+    const deploy = contrato.deploy({
+        data: storage_licitacoes_bytecode,
+        arguments: []
+    });
+    const estimatedGas = await deploy.estimateGas();
+    const encodedAbi = deploy.encodeABI();
+
+    const gasPrice = await web3.eth.getGasPrice();
 
     const objetoTransacao = {
-        data: deploy,
         from: from,
-        gas: 2273364,
-        gasPrice: 0
+        data: encodedAbi,
+        gas: estimatedGas,
+        gasPrice: gasPrice
     };
 
-    const rawTx = await assinarTransacao(objetoTransacao, privateKey);
-    return await web3.eth.sendSignedTransaction(rawTx);
+    const signedTransaction = await assinarTransacao(
+        objetoTransacao,
+        privateKey
+    );
+    const retorno = await web3.eth.sendSignedTransaction(signedTransaction);
+    return retorno.contractAddress;
 }
